5edd157a10c58183ee68d2c8e2180774b0167758
ducnnn <ducnn.230288@gmail.com>
2023-09-12 07:10:41 +0700

    init source

--- src/main.ts
+++ src/main.ts
@@ -11,12 +11,23 @@ animationSlide(document.getElementById('title')!, 0);
 GLightbox({});
 
 // sweetalert2 11
-// import { Message } from './message.ts'
+import { Message } from './message.ts'
 // document.getElementById('success')!.addEventListener('click', () => Message.success({ text: 'Success' }))
 // document.getElementById('warning')!.addEventListener('click', () => Message.warning({ text: 'Warning' }))
 // document.getElementById('confirm')!.addEventListener('click', () => Message.confirm({ text: 'Confirm' }))
 // document.getElementById('error')!.addEventListener('click', () => Message.error({ text: 'Error' }))
-
+// handle-dialog
+Array.from(document.getElementsByClassName('handle-dialog')).forEach((el) => el.addEventListener('click', () =>  {
+  const temp = document.getElementsByTagName("template")[0];
+  const item = temp.content.querySelector("div");
+  if (item) {
+    const a = document.importNode(item, true);
+    a.textContent += 'tesst';
+    const tmp = document.createElement("div");
+    tmp.appendChild(a);
+    Message.html(tmp.innerHTML);
+  }
+}))
 
 // import { setupCounter } from './counter.ts'
 // setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
