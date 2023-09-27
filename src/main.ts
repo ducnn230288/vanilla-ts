53e1f0165c7e5617f71a0d584ab4c19f587cc387
HienTran1108 <hientran11082k@gmail.com>
2023-09-27 14:39:08 +0700

    27/09

--- src/main.ts
+++ src/main.ts
@@ -9,36 +9,35 @@ setupSwiper();
 import {setupMenuMobile } from './menu-mobile.ts';
 setupMenuMobile(document.getElementsByClassName('handle-menu'));
 
-Array.from(document.getElementsByClassName('handle-dialog')).forEach((el) => el.addEventListener('click', () =>  {
-  const temp = document.getElementById("template-1");
-  if (temp) {
-    const a = document.importNode(temp, true);
-    a.innerHTML = a.innerHTML.replace(/{{test}}/g, 'TEST')
-    Message.html(a.innerHTML);
-  }
-}))
+// Array.from(document.getElementsByClassName('handle-dialog')).forEach((el) => el.addEventListener('click', () =>  {
+//   const temp = document.getElementById("template-1");
+//   if (temp) {
+//     const a = document.importNode(temp, true);
+//     a.innerHTML = a.innerHTML.replace(/{{test}}/g, 'TEST')
+//     Message.html(a.innerHTML);
+//   }
+// }))
 
 // glightbox 3
 GLightbox({});
 
 // echarts 5
-import { setupEchart } from './echart.ts'
-setupEchart();
+// import { setupEchart } from './echart.ts'
+// setupEchart();
 
 // inputmask 5
 Inputmask().mask(document.querySelectorAll("input"));
 
 
 // sweetalert2 11
-import { Message } from './message.ts'
-document.getElementById('success')!.addEventListener('click', () => Message.success({text: 'Success'}))
-document.getElementById('warning')!.addEventListener('click', () => Message.warning({text: 'Warning'}))
-document.getElementById('confirm')!.addEventListener('click', () => Message.confirm({text: 'Confirm'}))
-document.getElementById('error')!.addEventListener('click', () => Message.error({text: 'Error'}))
+// import { Message } from './message.ts'
+// document.getElementById('success')!.addEventListener('click', () => Message.success({text: 'Success'}))
+// document.getElementById('warning')!.addEventListener('click', () => Message.warning({text: 'Warning'}))
+// document.getElementById('confirm')!.addEventListener('click', () => Message.confirm({text: 'Confirm'}))
+// document.getElementById('error')!.addEventListener('click', () => Message.error({text: 'Error'}))
 
 
 import { 
-  setupCounter,
   toggleDropDown, 
   toggleDropList, 
   chooseItem, searchItem, 
@@ -58,8 +57,7 @@ import {
   dropPage,
   closeMenu,
   openMenu
-  } from './counter.ts'
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+} from './togglemenu.ts'
 toggleDropDown(document.querySelector<HTMLButtonElement>('#menubtn')!)
 toggleDropList(document.querySelector<HTMLButtonElement>('#btnlist')!)
 chooseItem(document.querySelector<HTMLButtonElement>('#btnlist')!)
