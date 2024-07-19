0d5e626e80af78d36c677cb93fecb3f05cf11a92
ducnn230288 <ducnn230288@gmail.com>
2024-07-20 05:19:54 +0700

    mega-menu

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,35 @@
+/**
+	Template Name    : Theme
+	Author			     : ducnn230288
+	Version			     : 1.0
+	File Name	       : main.js
+	Author Portfolio : https://github.com/ducnn230288
+
+	Core script to handle the entire theme and core functions
+**/
+
 import './less.less';
+
+const Theme = (() => {
+  /* Function ============ */
+  return {
+    init: () => {
+      console.log('init');
+    },
+
+    load: () => {
+      console.log('load');
+    },
+
+    scroll: () => {
+      console.log('scroll');
+    },
+
+    resize: () => {
+      console.log('resize');
+    },
+  };
+})();
 
 // sweetalert2 11
 import { API } from './api';
@@ -21,14 +52,24 @@ setupFormValid();
 import { setupMenuMobile } from './menu-mobile';
 setupMenuMobile(document.getElementsByClassName('handle-menu'));
 // swiper 10
-import { setupSwiper } from './swiper';
-setupSwiper();
+// import { setupSwiper } from './swiper';
+// setupSwiper();
 
 // echarts 5
 import { setupEchart } from './echart';
 setupEchart();
 
+/* Document.ready Start */
+document.addEventListener('DOMContentLoaded', () => {
+  Theme.init();
+});
+/* Document.ready END */
+
+/* Window Load START */
 window.onload = async () => {
+  Theme.load();
+
+  gsap.to(document.querySelector('.preloader'), { opacity: '0', display: 'none', duration: 0.5 });
   const test = await API.post({
     url: '/authentication/jwt/login',
     values: {
@@ -42,3 +83,24 @@ window.onload = async () => {
   });
   console.log(test);
 };
+/*  Window Load END */
+
+/* Window Resize START */
+window.addEventListener(
+  'resize',
+  () => {
+    Theme.resize();
+  },
+  true,
+);
+/*  Window Resize END */
+
+/* Window Resize START */
+window.addEventListener(
+  'scroll',
+  () => {
+    Theme.scroll();
+  },
+  true,
+);
+/*  Window Resize END */
